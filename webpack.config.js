d1f505567ef74346daf0381cfa2dc76623069208
qqq537 <920064577@qq.com>
2018-09-26 17:08:00 +0800

    123

--- webpack.config.js
+++ webpack.config.js
@@ -5,5 +5,11 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js'
+    },
+    module: {
+        rules: [{
+            test: /\.css$/,
+            use: ['style-loader', 'css-loader']
+        }]
     }
 };
