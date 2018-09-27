3333be9de39736eee17c7e9c2c8f4b86ce57c471
qqq537 <920064577@qq.com>
2018-09-27 17:31:52 +0800

    223

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import _ from 'lodash';
 import './style.css';
-import icon from './share-icon.png';
+// import icon from './share-icon.png';
+import printMe from './print'
 
 function component() {
     var element = document.createElement('div');
@@ -9,12 +10,28 @@ function component() {
     // Lodash, now imported by this script
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
     // 设置class样式
-    element.classList.add('hello');
+    // element.classList.add('hello');
     // 导入图片
-    var image = new Image();
-    image.src = icon;
-    element.appendChild(image);
+    // var image = new Image();
+    // image.src = icon;
+    // element.appendChild(image);
+
+    var btn = document.createElement('button');
+    btn.innerHTML = 'Click me and check the console!';
+    btn.onclick = printMe;
+    element.appendChild(btn);
     return element;
 }
+let element = component();
 
-document.body.appendChild(component());
+document.body.appendChild(element);
+
+if (module.hot) {
+    module.hot.accept('./print.js', function () {
+        console.log('Accepting the updated printMe module!');
+        printMe();
+        document.body.removeChild(element);
+        element = component();
+        document.body.appendChild(element);
+    });
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,30 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const webpack = require('webpack');
 
 module.exports = {
-    entry: './src/index.js',
+    // entry: './src/index.js',
+    entry: {
+        app: './src/index.js',
+        // print: './src/print.js'
+    },
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js'
+        filename: '[name].bundle.js', // filename: 'bundle.js'
     },
+    devServer: {
+        contentBase: './dist',
+        hot: true
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Hot Module Replacement'
+        }),
+        new CleanWebpackPlugin(['dist']),
+        new webpack.NamedModulesPlugin(),
+        new webpack.HotModuleReplacementPlugin()
+    ],
     module: {
         rules: [{
             test: /\.css$/,
